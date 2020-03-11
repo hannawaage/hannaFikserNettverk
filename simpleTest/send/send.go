dadd9f563a4138d263a9aabecc4220dca246eab3
Hanna Waage Hjelmeland <hannawh@stud.ntnu.no>
2020-03-11 19:11:03 +0100

    Transmit and receive in one file, i.e. send.go

--- simpleTest/send/send.go
+++ simpleTest/send/send.go
@@ -1,33 +1,63 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"time"
 
 	"../network/bcast"
+	"../network/localip"
 )
 
 type Message struct {
-	Note string
-	Id   int
+	Note    string
+	Id      int
+	Receipt bool
+	LocalIP string
 }
 
 func main() {
-	sendChn := make(chan Message)
+	var id string
 
-	go bcast.Transmitter(16571, sendChn)
+	flag.StringVar(&id, "id", "", "id of this peer")
+	flag.Parse()
+
+	port := 16576
+
+	sendChn := make(chan Message)
+	recChn := make(chan Message)
+
+	go bcast.Transmitter(port, sendChn)
+	go bcast.Receiver(port, recChn)
+
+	localIP, err := localip.LocalIP()
+	if err != nil {
+		fmt.Println(err)
+		localIP = "DISCONNECTED"
+	}
+
 	go func() {
-		msg := Message{"Heisann, sender mld nr ", 0}
+
+		msg := Message{"hello hello, iteration is ", 0, false, localIP}
 		for {
 			sendChn <- msg
 			msg.Id++
 			time.Sleep(1 * time.Second)
 		}
 	}()
+
 	for {
-		/*
-			select {
-			case outgoing := <-sendChn:
-				fmt.Printf("Prepared for sending: %#v\n", outgoing)
-			}*/
+		select {
+		case incomming := <-recChn:
+			if !incomming.Receipt && (incomming.LocalIP == localIP) {
+				fmt.Printf("Received: %#v\n", incomming.Note)
+				msg := Message{"This is a receipt of message ", incomming.Id, true, localIP}
+				sendChn <- msg
+			} else {
+				fmt.Printf("Received receipt on message %#v\n", incomming.Id, incomming.LocalIP)
+			}
+			/*case r := <-recReceiptCh:
+			fmt.Printf("Received Receipt from", r)*/
+		}
 	}
 }
